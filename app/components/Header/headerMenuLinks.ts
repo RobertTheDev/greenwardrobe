8cd376d9abb0c2b5d6e411bab9c4de05b054b543
Robert Hawker <robertthedev@gmail.com>
2025-01-14 18:02:25 +0000

    chore: defined header menu links

---
+++ app/components/Header/headerMenuLinks.ts
@@ -0,0 +1,28 @@
+const headerMenuLinks = [
+  {
+    name: 'Women',
+    slug: 'women',
+  },
+  {
+    name: 'Men',
+    slug: 'men',
+  },
+  {
+    name: 'Kids',
+    slug: 'kids',
+  },
+  {
+    name: 'Baby & Toddler',
+    slug: 'baby-toddler',
+  },
+  {
+    name: 'Brands',
+    slug: 'brands',
+  },
+  {
+    name: 'Materials',
+    slug: 'materials',
+  },
+];
+
+export default headerMenuLinks;
