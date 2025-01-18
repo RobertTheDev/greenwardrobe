a22ddadf72736cb052a5fb3df0fa5cdbf65ed70b
Robert Hawker <robertthedev@gmail.com>
2025-01-18 00:33:50 +0000

    feat: defined collections data

---
+++ app/data/collections.ts
@@ -0,0 +1,30 @@
+import ICollection from '~/interfaces/Collection';
+
+const collections: ICollection[] = [
+  {
+    id: '1',
+    slug: 'men',
+    name: 'Men',
+    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+  },
+  {
+    id: '2',
+    slug: 'women',
+    name: 'Women',
+    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+  },
+  {
+    id: '3',
+    slug: 'kids',
+    name: 'Kids',
+    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+  },
+  {
+    id: '4',
+    slug: 'toddler',
+    name: 'Toddler',
+    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+  },
+];
+
+export default collections;
