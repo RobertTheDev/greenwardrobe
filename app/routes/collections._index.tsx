d4c408fb060c8643a62aac1e7fff56f676050b85
Robert Hawker <robertthedev@gmail.com>
2025-01-18 01:13:44 +0000

    feat: added and mapped collections data as collection card components

--- app/routes/collections._index.tsx
+++ app/routes/collections._index.tsx
@@ -1,4 +1,6 @@
 import { MetaFunction } from '@remix-run/react';
+import CollectionCard from '~/components/CollectionCard';
+import collections from '~/data/collections';
 import {
   generateMetaDescription,
   generateMetaTitle,
@@ -17,7 +19,18 @@ export const meta: MetaFunction = () => {
 export default function CollectionsPage() {
   return (
     <div>
-      <h1>Collections</h1>
+      <div className="mx-auto max-w-xl py-4 font-sans md:max-w-4xl lg:max-w-6xl">
+        <h2 className="mb-10 text-center text-2xl font-bold text-gray-800 sm:text-3xl">
+          Collections
+        </h2>
+        <div className="grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-3 lg:grid-cols-4">
+          {collections.map((collection) => {
+            return (
+              <CollectionCard key={collection.id} collection={collection} />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
