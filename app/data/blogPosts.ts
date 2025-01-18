5e60cd6d40fe819cf15e123ec8bb7ce100c71e3c
Robert Hawker <robertthedev@gmail.com>
2025-01-18 00:34:18 +0000

    feat: defined blog posts data

---
+++ app/data/blogPosts.ts
@@ -0,0 +1,42 @@
+import { IBlogPost } from '~/interfaces/BlogPost';
+
+const blogPosts: IBlogPost[] = [
+  {
+    id: '1',
+    slug: 'lorem-ipsum-dolor',
+    title: 'Lorem Ipsum Dolor',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...',
+    date: '08 April 2024',
+    image: 'https://readymadeui.com/images/food.webp',
+  },
+  {
+    id: '2',
+    slug: 'consectetur-adipiscing',
+    title: 'Consectetur Adipiscing',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...',
+    date: '08 April 2024',
+    image: 'https://readymadeui.com/images/food11.webp',
+  },
+  {
+    id: '3',
+    slug: 'sed-do-eiusmod',
+    title: 'Lorem Ipsum Sit Amet',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...',
+    date: '08 April 2024',
+    image: 'https://readymadeui.com/images/food22.webp',
+  },
+  {
+    id: '4',
+    slug: 'tempor-incididunt-ut',
+    title: 'Lorem Ipsum Sit Amet',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...',
+    date: '08 April 2024',
+    image: 'https://readymadeui.com/images/food33.webp',
+  },
+];
+
+export default blogPosts;
