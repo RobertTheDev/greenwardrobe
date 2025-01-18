81b12702c572cd3922e8cc623d3d8d8363e679d6
Robert Hawker <robertthedev@gmail.com>
2025-01-18 01:03:31 +0000

    feat: added and fetched the blog post from the data

--- app/routes/blog.$articleHandle.tsx
+++ app/routes/blog.$articleHandle.tsx
@@ -1,4 +1,5 @@
-import { MetaFunction } from '@remix-run/react';
+import { MetaFunction, useParams } from '@remix-run/react';
+import blogPosts from '~/data/blogPosts';
 import {
   generateMetaDescription,
   generateMetaTitle,
@@ -15,9 +16,35 @@ export const meta: MetaFunction = () => {
 };
 
 export default function BlogArticlePage() {
+  const params = useParams();
+
+  const blogPost = blogPosts.find((post) => post.slug === params.articleHandle);
+
+  if (!blogPost) {
+    return (
+      <div>
+        <p>Not found</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Blog</h1>
+      <div className="relative bg-gradient-to-r from-purple-900 to-indigo-800 py-52 font-[sans-serif]">
+        <div className="absolute inset-0">
+          <img
+            src={blogPost.image}
+            alt=""
+            className="h-full w-full object-cover opacity-60"
+          />
+        </div>
+
+        <div className="relative z-10 mx-auto max-w-screen-xl px-8 text-center text-white">
+          <h1 className="mb-6 text-4xl font-extrabold leading-tight md:text-5xl">
+            {blogPost.title}
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
