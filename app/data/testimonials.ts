d2334acb51f61f7c2c988c564bc4018923795750
Robert Hawker <robertthedev@gmail.com>
2025-01-18 00:32:47 +0000

    feat: defined testimonials data

---
+++ app/data/testimonials.ts
@@ -0,0 +1,30 @@
+import ITestimonial from '~/interfaces/Testimonial';
+
+const testimonials: ITestimonial[] = [
+  {
+    name: 'John Doe',
+    title: 'Founder of Rubik',
+    image: 'https://readymadeui.com/team-2.webp',
+    rating: 5,
+    content:
+      'The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.',
+  },
+  {
+    name: 'Mark Adair',
+    title: 'Founder of Alpha',
+    image: 'https://readymadeui.com/team-1.webp',
+    rating: 5,
+    content:
+      'The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.',
+  },
+  {
+    name: 'Simon Konecki',
+    title: 'Founder of Labar',
+    image: 'https://readymadeui.com/team-4.webp',
+    rating: 4,
+    content:
+      'The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.',
+  },
+];
+
+export default testimonials;
