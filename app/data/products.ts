66766fbd26c1b07973aff24b6b4ecfad9459bae3
Robert Hawker <robertthedev@gmail.com>
2025-01-18 00:33:21 +0000

    feat: defined producta data

---
+++ app/data/products.ts
@@ -0,0 +1,62 @@
+import IProduct from '~/interfaces/Product';
+
+const products: IProduct[] = [
+  {
+    id: '1',
+    name: 'Lexicon Luxe - Tshirt',
+    price: '$100.00',
+    image: 'https://readymadeui.com/images/fashion-img-1.webp',
+    slug: 'lexicon-luxe-tshirt',
+  },
+  {
+    id: '2',
+    name: 'Adjective Attire - Tshirt',
+    price: '$120.00',
+    image: 'https://readymadeui.com/images/fashion-img-2.webp',
+    slug: 'adjective-attire-tshirt',
+  },
+  {
+    id: '3',
+    name: 'ThreadCraft Vibes - Tshirt',
+    price: '$140.00',
+    image: 'https://readymadeui.com/images/fashion-img-3.webp',
+    slug: 'threadcraft-vibes-tshirt',
+  },
+  {
+    id: '4',
+    name: 'Verbal Vogue Tees - Tshirt',
+    price: '$120.00',
+    image: 'https://readymadeui.com/images/fashion-img-4.webp',
+    slug: 'verbal-vogue-tees-tshirt',
+  },
+  {
+    id: '5',
+    name: 'Jargon Jungle - Tshirt',
+    price: '$150.00',
+    image: 'https://readymadeui.com/images/fashion-img-5.webp',
+    slug: 'jargon-jungle-tshirt',
+  },
+  {
+    id: '6',
+    name: 'Syntax Style - Tshirt',
+    price: '$130.00',
+    image: 'https://readymadeui.com/images/fashion-img-6.webp',
+    slug: 'syntax-style-tshirt',
+  },
+  {
+    id: '7',
+    name: 'Grammar Garb - Tshirt',
+    price: '$110.00',
+    image: 'https://readymadeui.com/images/fashion-img-7.webp',
+    slug: 'grammar-garb-tshirt',
+  },
+  {
+    id: '8',
+    name: 'Linguistic Look - Tshirt',
+    price: '$160.00',
+    image: 'https://readymadeui.com/images/fashion-img-9.webp',
+    slug: 'linguistic-look-tshirt',
+  },
+];
+
+export default products;
