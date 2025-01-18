1e84f892c7a33e9f23cb0fc7c1dd36b84c4e6239
Robert Hawker <robertthedev@gmail.com>
2025-01-18 01:15:32 +0000

    feat: added contact form component to contact page

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,5 +1,4 @@
 import { MetaFunction } from '@remix-run/react';
-import ContactForm from '~/components/ContactForm';
 import {
   generateMetaDescription,
   generateMetaTitle,
@@ -17,9 +16,63 @@ export const meta: MetaFunction = () => {
 
 export default function ContactPage() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <ContactForm />
-    </div>
+    <main className="flex overflow-hidden">
+      <div className="hidden flex-1 lg:block">
+        <img
+          alt=""
+          src={'/contact-page-photo.jpg'}
+          className="h-screen w-full object-cover"
+        />
+      </div>
+      <div className="flex-1 py-12 lg:flex lg:h-screen lg:justify-center lg:overflow-auto">
+        <div className="mx-auto max-w-lg flex-1 px-4 text-gray-600">
+          <div>
+            <h3 className="text-3xl font-semibold text-gray-800 sm:text-4xl">
+              Get in touch
+            </h3>
+            <p className="mt-3">
+              We’d love to hear from you! Please fill out the form bellow.
+            </p>
+          </div>
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="mt-12 space-y-5 lg:pb-12"
+          >
+            <div>
+              <label htmlFor="fullName" className="font-medium">
+                Full name
+              </label>
+              <input
+                type="text"
+                required
+                className="mt-2 w-full rounded-md border bg-transparent px-3 py-2 text-gray-500 shadow-sm outline-none focus:border-gray-800"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="font-medium">
+                Email
+              </label>
+              <input
+                type="email"
+                required
+                className="mt-2 w-full rounded-md border bg-transparent px-3 py-2 text-gray-500 shadow-sm outline-none focus:border-gray-800"
+              />
+            </div>
+            <div>
+              <label htmlFor="message" className="font-medium">
+                Message
+              </label>
+              <textarea
+                required
+                className="mt-2 h-36 w-full resize-none appearance-none rounded-md border bg-transparent px-3 py-2 shadow-sm outline-none focus:border-gray-800"
+              />
+            </div>
+            <button className="w-full rounded-md bg-gray-800 p-4 font-medium text-white duration-150 hover:bg-gray-700 active:bg-gray-900">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
   );
 }
