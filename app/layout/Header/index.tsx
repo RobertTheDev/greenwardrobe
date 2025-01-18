32ca427f29ac906f25449ae679a46a3a6f1405fa
Robert Hawker <robertthedev@gmail.com>
2025-01-18 13:35:08 +0000

    feat: added navigation links to the header controls

--- app/layout/Header/index.tsx
+++ app/layout/Header/index.tsx
@@ -3,6 +3,8 @@ import { LuCircleUser, LuSearch, LuShoppingBasket } from 'react-icons/lu';
 import collections from '~/data/collections';
 
 export default function Header() {
+  const signedIn = true;
+
   return (
     <header className="sticky top-0 z-[99] flex min-h-16 border-b bg-white px-4 py-3 font-[sans-serif] tracking-wide sm:px-6">
       <div className="mx-auto flex w-full max-w-screen-xl">
@@ -39,27 +41,37 @@ export default function Header() {
             </ul>
           </div>
           <div className="ml-auto flex items-center gap-x-6 gap-y-4">
-            <div className="flex max-w-52 overflow-hidden rounded-full border border-black bg-gray-50 px-4 py-2.5 focus-within:border-gray-400 focus-within:bg-transparent max-lg:hidden">
+            <form
+              method="get"
+              action="/search"
+              className="flex max-w-52 overflow-hidden rounded-full border border-black bg-gray-50 px-4 py-2.5 focus-within:border-gray-400 focus-within:bg-transparent max-lg:hidden"
+            >
               <input
                 type="text"
+                name="q"
                 placeholder="Search something..."
                 className="w-full bg-transparent pr-2 text-sm outline-none"
               />
-              <LuSearch size={20} />
-            </div>
+              <button type="submit">
+                <LuSearch size={20} />
+              </button>
+            </form>
             <div className="flex items-center space-x-6 sm:space-x-8">
               <button id="toggleOpen" className="lg:hidden">
                 <LuSearch size={28} />
               </button>
-              <button className="flex cursor-pointer flex-col items-center justify-center gap-0.5">
+              <Link
+                to="/basket"
+                className="flex cursor-pointer flex-col items-center justify-center gap-0.5"
+              >
                 <div className="relative">
                   <LuShoppingBasket size={28} />
                   <span className="absolute right-0 top-0 -mr-1 -mt-1 rounded-full bg-red-500 px-1 py-0 text-xs text-white">
                     2
                   </span>
                 </div>
-              </button>
-              <Link to="/sign-in">
+              </Link>
+              <Link to={signedIn ? '/account' : '/sign-in'}>
                 <LuCircleUser size={28} />
               </Link>
             </div>
