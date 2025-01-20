b3a68494f30feb2f669516798976358e93bd834a
Robert Hawker <robertthedev@gmail.com>
2025-01-20 03:36:39 +0000

    feat: added footer navigation links to footer component

--- app/layout/Footer/index.tsx
+++ app/layout/Footer/index.tsx
@@ -1,158 +1,86 @@
 import { Link } from '@remix-run/react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa6';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import footerNavigationLinks from './footerNavigationLinks';
 
-const footerNavigationLinks = [
-  {
-    title: 'Shop',
-    links: [
-      {
-        title: 'About',
-        url: '/about',
-      },
-      {
-        title: 'Collections',
-        url: '/collections',
-      },
-      {
-        title: 'Home',
-        url: '/',
-      },
-    ],
-  },
-  {
-    title: 'Company',
-    links: [
-      {
-        title: 'Contact',
-        url: '/contact',
-      },
-      {
-        title: 'FAQ',
-        url: '/faq',
-      },
-      {
-        title: 'Blog',
-        url: '/blog',
-      },
-    ],
-  },
-  {
-    title: 'Information',
-    links: [
-      {
-        title: 'Returns Policy',
-        url: '/legal/returns-policy',
-      },
-      {
-        title: 'Privacy Policy',
-        url: '/legal/privacy-policy',
-      },
-      {
-        title: 'Terms & Conditions',
-        url: '/legal/terms-and-conditions',
-      },
-    ],
-  },
-];
+function FooterNavigationMenu({
+  props,
+}: {
+  props: { title: string; links: { title: string; url: string }[] };
+}) {
+  return (
+    <div className="text-left lg:mx-auto">
+      <h4 className="mb-7 text-base font-semibold text-neutral-900">
+        {props.title}
+      </h4>
+      <ul className="text-sm transition-all duration-500">
+        {props.links.map((link) => {
+          return (
+            <li key={link.url} className="mb-6">
+              <Link
+                to={link.url}
+                className="text-sm text-neutral-600 hover:underline"
+              >
+                {link.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default function Footer() {
   return (
-    <footer className="relative border-t bg-gradient-to-r px-10 pb-6 pt-12 font-sans tracking-wide">
-      <div className="mx-auto max-w-screen-xl">
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
-          <div>
-            <h2 className="mb-4 text-sm font-semibold uppercase">
-              {footerNavigationLinks[0].title}
-            </h2>
-            <ul className="space-y-3">
-              {footerNavigationLinks[0].links.map((link) => {
-                return (
-                  <li key={link.url}>
-                    <Link
-                      to={link.url}
-                      className="text-sm text-gray-400 transition-all hover:text-white"
-                    >
-                      {link.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          <div>
-            <h2 className="mb-4 text-sm font-semibold uppercase">
-              {footerNavigationLinks[1].title}
-            </h2>
-            <ul className="space-y-3">
-              {footerNavigationLinks[1].links.map((link) => {
-                return (
-                  <li key={link.url}>
-                    <Link
-                      to={link.url}
-                      className="text-sm text-gray-400 transition-all hover:text-white"
-                    >
-                      {link.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          <div>
-            <h2 className="mb-4 text-sm font-semibold uppercase">
-              {footerNavigationLinks[2].title}
-            </h2>
-            <ul className="space-y-3">
-              {footerNavigationLinks[2].links.map((link) => {
-                return (
-                  <li key={link.url}>
-                    <Link
-                      to={link.url}
-                      className="text-sm text-gray-400 transition-all hover:text-white"
-                    >
-                      {link.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          <div className="flex items-center max-sm:-order-1 lg:justify-center">
-            <Link to="/">
+    <footer className="w-full border-t">
+      <div className="mx-auto max-w-7xl">
+        <div className="grid grid-cols-2 gap-3 gap-y-8 py-10 max-sm:mx-auto max-sm:max-w-sm sm:grid-cols-4 md:gap-8 lg:grid-cols-6">
+          <div className="col-span-full mb-10 lg:col-span-2 lg:mb-0">
+            <a
+              href="https://pagedone.io/"
+              className="flex justify-center lg:justify-start"
+            >
               <img
                 src="/desktop-logo.jpg"
-                alt="logo"
-                className="w-40 sm:w-56"
+                alt="Pagedone"
+                className="h-9 w-auto"
               />
-            </Link>
+            </a>
+            <p className="mt-6 text-center text-sm text-gray-500 lg:max-w-xs lg:text-left">
+              Trusted in more than 100 countries & 5 million customers. Have any
+              query ?
+            </p>
           </div>
+
+          {footerNavigationLinks.map((props) => {
+            return <FooterNavigationMenu key={props.title} props={props} />;
+          })}
         </div>
-        <hr className="mb-6 mt-12 border-gray-600" />
-        <div className="flex flex-wrap gap-6 sm:justify-between">
-          <div className="flex space-x-5">
-            <a
-              href="https://www.github.com/robertthedev"
-              target="_blank"
-              rel="noreferrer"
-              className="text-sm text-gray-400 transition-all hover:text-white"
-            >
-              <FaGithub size={24} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/robertthedev"
-              target="_blank"
-              rel="noreferrer"
-              className="text-sm text-gray-400 transition-all hover:text-white"
-            >
-              <FaLinkedin size={24} />
-            </a>
+
+        <div className="border-t border-gray-200 py-5">
+          <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between">
+            <span className="text-sm text-gray-500">
+              © {new Date().getFullYear()} Green Wardrobe, All rights reserved.
+            </span>
+            <div className="mt-4 flex space-x-2 sm:justify-center lg:mt-0">
+              <a
+                href="https://www.github.com/robertthedev"
+                target="_blank"
+                className="flex h-9 w-9 items-center justify-center rounded-full text-gray-500"
+                rel="noreferrer"
+              >
+                <FaGithub size={24} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/robertthedev"
+                target="_blank"
+                className="flex h-9 w-9 items-center justify-center rounded-full text-gray-500"
+                rel="noreferrer"
+              >
+                <FaLinkedin size={24} />
+              </a>
+            </div>
           </div>
-          <p className="text-sm text-gray-400">
-            © Green Wardrobe. All rights reserved.
-          </p>
         </div>
       </div>
     </footer>
