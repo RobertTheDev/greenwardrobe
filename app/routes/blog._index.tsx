f6ce7bb661cd0ab3d26b3da57be5b1dbec3aa0cf
Robert Hawker <robertthedev@gmail.com>
2025-01-20 03:33:28 +0000

    feat: created and mapped blog post card component

--- app/routes/blog._index.tsx
+++ app/routes/blog._index.tsx
@@ -1,6 +1,6 @@
 import { MetaFunction } from '@remix-run/react';
-import BlogCard from '~/components/BlogCard';
-import blogPosts from '~/data/blogPosts';
+import BlogPostCard from '~/components/BlogPostCard';
+import { blogPosts } from '~/data/blogPosts';
 import {
   generateMetaDescription,
   generateMetaTitle,
@@ -18,16 +18,14 @@ export const meta: MetaFunction = () => {
 
 export default function BlogArticlesPage() {
   return (
-    <div className="p-4 font-[sans-serif]">
-      <div className="mx-auto max-w-6xl max-lg:max-w-3xl max-sm:max-w-sm">
-        <div className="mx-auto max-w-md">
-          <h2 className="mb-12 text-center text-3xl font-extrabold leading-10 text-gray-800">
-            Stay updated with the latest blog posts.
-          </h2>
-        </div>
-        <div className="grid grid-cols-1 gap-6 max-sm:gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          {blogPosts.map((post) => {
-            return <BlogCard key={post.id} post={post} />;
+    <div className="py-24">
+      <div className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8">
+        <h2 className="font-manrope mb-8 text-center text-2xl font-bold leading-10 text-black min-[400px]:text-4xl">
+          Latest Blog
+        </h2>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {blogPosts.map((props) => {
+            return <BlogPostCard key={props.id} props={props} />;
           })}
         </div>
       </div>
