5466a69db8417d7daadf2065dfcb8e3ff24c31f6
Robert Hawker <robertthedev@gmail.com>
2025-01-20 03:27:40 +0000

    feat: added products to the collectons data array

--- app/data/collections.ts
+++ app/data/collections.ts
@@ -5,25 +5,197 @@ const collections: ICollection[] = [
     id: '1',
     slug: 'men',
     name: 'Men',
-    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+    image:
+      'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_81.jpg?v=1709909490',
+    products: [
+      {
+        id: '1',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_81.jpg?v=1709909490',
+        name: 'Roadster',
+        slug: 'roadster',
+        description: 'Black Solid Round Neck',
+        price: 80,
+        discount: 100,
+      },
+      {
+        id: '2',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_91.jpg?v=1709910354',
+        name: 'Blue Blazer',
+        slug: 'blue-blazer',
+        description: 'Men’s clothes',
+        price: 120,
+        discount: 149,
+      },
+      {
+        id: '3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_92.jpg',
+        name: 'Black Shirt',
+        slug: 'black-shirt',
+        description: 'Shirts',
+        price: 75,
+        discount: 100,
+      },
+      {
+        id: '4',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_-_2024-03-11T143220.000.jpg?v=1710169942',
+        name: 'Beige T-shirt',
+        slug: 'beige-t-shirt',
+        description: 'Women’s clothes',
+        price: 75,
+        discount: 75,
+      },
+    ],
   },
   {
     id: '2',
     slug: 'women',
     name: 'Women',
-    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+    image:
+      'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_31.jpg',
+    products: [
+      {
+        id: '1',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_81.jpg?v=1709909490',
+        name: 'Roadster',
+        slug: 'roadster',
+        description: 'Black Solid Round Neck',
+        price: 80,
+        discount: 100,
+      },
+      {
+        id: '2',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_91.jpg?v=1709910354',
+        name: 'Blue Blazer',
+        slug: 'blue-blazer',
+        description: 'Men’s clothes',
+        price: 120,
+        discount: 149,
+      },
+      {
+        id: '3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_92.jpg',
+        name: 'Black Shirt',
+        slug: 'black-shirt',
+        description: 'Shirts',
+        price: 75,
+        discount: 100,
+      },
+      {
+        id: '4',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_-_2024-03-11T143220.000.jpg?v=1710169942',
+        name: 'Beige T-shirt',
+        slug: 'beige-t-shirt',
+        description: 'Women’s clothes',
+        price: 75,
+        discount: 75,
+      },
+    ],
   },
   {
     id: '3',
     slug: 'kids',
     name: 'Kids',
-    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+    image:
+      'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_-_2024-07-26T154917.311.jpg?v=1722001763',
+    products: [
+      {
+        id: '1',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_81.jpg?v=1709909490',
+        name: 'Roadster',
+        slug: 'roadster',
+        description: 'Black Solid Round Neck',
+        price: 80,
+        discount: 100,
+      },
+      {
+        id: '2',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_91.jpg?v=1709910354',
+        name: 'Blue Blazer',
+        slug: 'blue-blazer',
+        description: 'Men’s clothes',
+        price: 120,
+        discount: 149,
+      },
+      {
+        id: '3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_92.jpg',
+        name: 'Black Shirt',
+        slug: 'black-shirt',
+        description: 'Shirts',
+        price: 75,
+        discount: 100,
+      },
+      {
+        id: '4',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_-_2024-03-11T143220.000.jpg?v=1710169942',
+        name: 'Beige T-shirt',
+        slug: 'beige-t-shirt',
+        description: 'Women’s clothes',
+        price: 75,
+        discount: 75,
+      },
+    ],
   },
   {
     id: '4',
     slug: 'toddler',
     name: 'Toddler',
-    image: 'https://pagedone.io/asset/uploads/1695365240.png',
+    image:
+      'https://earthwardrobe.com/cdn/shop/products/BZ032_LS00_2021.jpg?crop=center&height=768&v=1710790198&width=640g',
+    products: [
+      {
+        id: '1',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_81.jpg?v=1709909490',
+        name: 'Roadster',
+        slug: 'roadster',
+        description: 'Black Solid Round Neck',
+        price: 80,
+        discount: 100,
+      },
+      {
+        id: '2',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_91.jpg?v=1709910354',
+        name: 'Blue Blazer',
+        slug: 'blue-blazer',
+        description: 'Men’s clothes',
+        price: 120,
+        discount: 149,
+      },
+      {
+        id: '3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_92.jpg',
+        name: 'Black Shirt',
+        slug: 'black-shirt',
+        description: 'Shirts',
+        price: 75,
+        discount: 100,
+      },
+      {
+        id: '4',
+        image:
+          'https://cdn.shopify.com/s/files/1/0251/9855/9279/files/Untitled_design_-_2024-03-11T143220.000.jpg?v=1710169942',
+        name: 'Beige T-shirt',
+        slug: 'beige-t-shirt',
+        description: 'Women’s clothes',
+        price: 75,
+        discount: 75,
+      },
+    ],
   },
 ];
 
