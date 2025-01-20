903ec62226fe3a941eef1be276fbeab63759c0fe
Robert Hawker <robertthedev@gmail.com>
2025-01-20 03:33:05 +0000

    feat: added discount and prce fields to product card component

--- app/components/ProductCard/index.tsx
+++ app/components/ProductCard/index.tsx
@@ -4,55 +4,49 @@ import IProduct from '~/interfaces/Product';
 export default function ProductCard({ product }: { product: IProduct }) {
   return (
     <Link to={`/products/${product.slug}`}>
-      <div className="group relative cursor-pointer overflow-hidden">
-        <div className="w-full overflow-hidden bg-gray-100">
+      <div className="box group mx-auto w-full max-w-[280px] sm:mr-0">
+        <div className="img-box flex h-auto w-full flex-col items-center justify-end overflow-hidden rounded-3xl">
           <img
             src={product.image}
-            alt="Product 1"
-            className="aspect-[3/4] w-full object-cover object-top transition-all duration-700 hover:scale-110"
+            alt=""
+            className="h-full w-full rounded-2xl object-cover transition-all duration-700 group-hover:scale-[1.05]"
           />
         </div>
-
-        <div className="relative p-4">
-          <div className="absolute left-0 right-0 z-10 flex w-full flex-wrap justify-between gap-2 px-4 pt-3 transition-all duration-500 group-hover:bottom-20 max-lg:bottom-20 max-lg:bg-white/60 max-lg:py-3 lg:bottom-5 lg:bg-white lg:opacity-0 lg:group-hover:opacity-100">
-            <button
-              type="button"
-              title="Add to wishlist"
-              className="border-none bg-transparent outline-none"
-            >
+        <div className="mt-5">
+          <div className="mb-3 flex items-center justify-between gap-3">
+            <div className="pro-name">
+              <h6 className="mb-2 text-base font-semibold leading-7 text-black">
+                {product.name}
+              </h6>
+              <p className="text-xs font-normal leading-5 text-gray-500">
+                {product.description}
+              </p>
+            </div>
+            <button className="group flex h-14 w-14 items-center justify-center rounded-full border border-gray-200 bg-transparent p-3 shadow-sm shadow-transparent transition-all duration-500 hover:border-gray-400 hover:bg-gray-100 hover:shadow-gray-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="inline-block h-5 w-5 fill-gray-800"
-                viewBox="0 0 64 64"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
               >
                 <path
-                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                  data-original="#000000"
-                />
-              </svg>
-            </button>
-            <button
-              type="button"
-              title="Add to cart"
-              className="border-none bg-transparent outline-none"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="inline-block h-5 w-5 fill-gray-800"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  d="M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm167 15c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0"
-                  data-original="#000000"
+                  d="M14.1211 5.12129L13.414 4.41418C12.7474 3.74752 12.414 3.41418 11.9998 3.41418C11.5856 3.41418 11.2523 3.74752 10.5856 4.41418L9.87849 5.12129M8.02802 21H15.9716C18.2908 21 19.4503 21 20.0509 20.2451C20.6514 19.4902 20.3907 18.3603 19.8692 16.1005L18.7153 11.1005C18.3721 9.61328 18.2005 8.86966 17.6539 8.43481C17.1073 7.99997 16.3441 7.99997 14.8178 7.99997H9.18186C7.65552 7.99997 6.89235 7.99997 6.34573 8.43481C5.79912 8.86966 5.62751 9.61328 5.2843 11.1005L4.13045 16.1005C3.60897 18.3603 3.34823 19.4902 3.94876 20.2451C4.54929 21 5.70886 21 8.02802 21Z"
+                  stroke="#111827"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </button>
           </div>
-          <div className="relative z-20 bg-white">
-            <h6 className="truncate text-sm font-semibold text-gray-800">
-              {product.name}
-            </h6>
-            <h6 className="mt-2 text-sm text-gray-600">{product.price}</h6>
+          <div className="flex gap-1">
+            <strong className="text-base font-semibold leading-7 text-indigo-600">
+              ${product.price}{' '}
+            </strong>
+            <p className="flex items-center text-xs font-normal leading-5 text-indigo-200">
+              <del className="text-indigo-200"> ${product.discount}</del>
+            </p>
           </div>
         </div>
       </div>
