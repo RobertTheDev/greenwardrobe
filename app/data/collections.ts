63a219b4a43dea42b2a4c5bd3fe5905c13da4350
Robert Hawker <robertthedev@gmail.com>
2025-01-21 02:44:54 +0000

    feat: added new products to collections

--- app/data/collections.ts
+++ app/data/collections.ts
@@ -170,6 +170,60 @@ const collections: ICollection[] = [
           },
         ],
       },
+      {
+        id: '4',
+        slug: 'roadster',
+        title: 'Women Embroidered A-line Kurta',
+        description:
+          'Women Embroidered Georgette A-line Kurta with Attached Dupatta in a rich maroon shade, featuring intricate embroidery, and a lightweight georgette fabric.',
+        price: {
+          current: 12,
+          previous: 16,
+          currency: 'USD',
+          taxIncluded: true,
+        },
+        productInformation: [
+          {
+            title: 'Product details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Vendor details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Return and exchange policy',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+        ],
+        colours: ['Blue', 'Green', 'Yellow', 'Orange', 'Purple'],
+        sizes: ['SM', 'MD', 'LG', 'XL', 'XXL'],
+        images: [
+          {
+            id: '1',
+            src: 'https://readymadeui.com/images/fashion-img-1.webp',
+            alt: 'Product',
+          },
+          {
+            id: '2',
+            src: 'https://readymadeui.com/images/fashion-img-2.webp',
+            alt: 'Product2',
+          },
+          {
+            id: '3',
+            src: 'https://readymadeui.com/images/fashion-img-3.webp',
+            alt: 'Product3',
+          },
+          {
+            id: '4',
+            src: 'https://readymadeui.com/images/fashion-img-4.webp',
+            alt: 'Product4',
+          },
+        ],
+      },
     ],
   },
   {
@@ -289,6 +343,60 @@ const collections: ICollection[] = [
       },
       {
         id: '3',
+        slug: 'roadster',
+        title: 'Women Embroidered A-line Kurta',
+        description:
+          'Women Embroidered Georgette A-line Kurta with Attached Dupatta in a rich maroon shade, featuring intricate embroidery, and a lightweight georgette fabric.',
+        price: {
+          current: 12,
+          previous: 16,
+          currency: 'USD',
+          taxIncluded: true,
+        },
+        productInformation: [
+          {
+            title: 'Product details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Vendor details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Return and exchange policy',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+        ],
+        colours: ['Blue', 'Green', 'Yellow', 'Orange', 'Purple'],
+        sizes: ['SM', 'MD', 'LG', 'XL', 'XXL'],
+        images: [
+          {
+            id: '1',
+            src: 'https://readymadeui.com/images/fashion-img-1.webp',
+            alt: 'Product',
+          },
+          {
+            id: '2',
+            src: 'https://readymadeui.com/images/fashion-img-2.webp',
+            alt: 'Product2',
+          },
+          {
+            id: '3',
+            src: 'https://readymadeui.com/images/fashion-img-3.webp',
+            alt: 'Product3',
+          },
+          {
+            id: '4',
+            src: 'https://readymadeui.com/images/fashion-img-4.webp',
+            alt: 'Product4',
+          },
+        ],
+      },
+      {
+        id: '4',
         slug: 'roadster',
         title: 'Women Embroidered A-line Kurta',
         description:
@@ -512,6 +620,60 @@ const collections: ICollection[] = [
           },
         ],
       },
+      {
+        id: '4',
+        slug: 'roadster',
+        title: 'Women Embroidered A-line Kurta',
+        description:
+          'Women Embroidered Georgette A-line Kurta with Attached Dupatta in a rich maroon shade, featuring intricate embroidery, and a lightweight georgette fabric.',
+        price: {
+          current: 12,
+          previous: 16,
+          currency: 'USD',
+          taxIncluded: true,
+        },
+        productInformation: [
+          {
+            title: 'Product details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Vendor details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Return and exchange policy',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+        ],
+        colours: ['Blue', 'Green', 'Yellow', 'Orange', 'Purple'],
+        sizes: ['SM', 'MD', 'LG', 'XL', 'XXL'],
+        images: [
+          {
+            id: '1',
+            src: 'https://readymadeui.com/images/fashion-img-1.webp',
+            alt: 'Product',
+          },
+          {
+            id: '2',
+            src: 'https://readymadeui.com/images/fashion-img-2.webp',
+            alt: 'Product2',
+          },
+          {
+            id: '3',
+            src: 'https://readymadeui.com/images/fashion-img-3.webp',
+            alt: 'Product3',
+          },
+          {
+            id: '4',
+            src: 'https://readymadeui.com/images/fashion-img-4.webp',
+            alt: 'Product4',
+          },
+        ],
+      },
     ],
   },
   {
@@ -631,6 +793,60 @@ const collections: ICollection[] = [
       },
       {
         id: '3',
+        slug: 'roadster',
+        title: 'Women Embroidered A-line Kurta',
+        description:
+          'Women Embroidered Georgette A-line Kurta with Attached Dupatta in a rich maroon shade, featuring intricate embroidery, and a lightweight georgette fabric.',
+        price: {
+          current: 12,
+          previous: 16,
+          currency: 'USD',
+          taxIncluded: true,
+        },
+        productInformation: [
+          {
+            title: 'Product details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Vendor details',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+          {
+            title: 'Return and exchange policy',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          },
+        ],
+        colours: ['Blue', 'Green', 'Yellow', 'Orange', 'Purple'],
+        sizes: ['SM', 'MD', 'LG', 'XL', 'XXL'],
+        images: [
+          {
+            id: '1',
+            src: 'https://readymadeui.com/images/fashion-img-1.webp',
+            alt: 'Product',
+          },
+          {
+            id: '2',
+            src: 'https://readymadeui.com/images/fashion-img-2.webp',
+            alt: 'Product2',
+          },
+          {
+            id: '3',
+            src: 'https://readymadeui.com/images/fashion-img-3.webp',
+            alt: 'Product3',
+          },
+          {
+            id: '4',
+            src: 'https://readymadeui.com/images/fashion-img-4.webp',
+            alt: 'Product4',
+          },
+        ],
+      },
+      {
+        id: '4',
         slug: 'roadster',
         title: 'Women Embroidered A-line Kurta',
         description:
