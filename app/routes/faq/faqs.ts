288e79fd2ac667a2a5d3a44f9bc66eba2576f897
Robert Hawker <robertthedev@gmail.com>
2025-01-21 17:22:36 +0000

    refactor: updated faqs data list

---
+++ app/routes/faq/faqs.ts
@@ -0,0 +1,24 @@
+const faqs = [
+  {
+    id: '1',
+    q: 'How do I find my purchase history?',
+    a: 'First log in and go to the account or purchase history page. Look for a list of your past purchases or orders, and click on any item to see more details.',
+  },
+  {
+    id: '2',
+    q: 'How do I change my privacy settings?',
+    a: 'To change your privacy settings, log in and go to the settings or account page. Look for an option to "Privacy" or "Security" and follow the instructions.',
+  },
+  {
+    id: '3',
+    q: 'How do I change my email address?',
+    a: 'To change your email address, log in and go to the settings or account page. Look for an option to "Change email" or "Edit account information" and follow the prompts. Be sure to save your changes before exiting.',
+  },
+  {
+    id: '4',
+    q: 'How do I update my profile information?',
+    a: 'To update your profile information, log in and go to the profile or account page. Look for an option to "Edit profile" or "Update information" and follow the prompts. Be sure to save your changes before exiting.',
+  },
+];
+
+export default faqs;
